5b15195c8fe54eb80816e3098f4ab1b2493637a9
Asaf Ravona <asaf.ravona@ladbrokescoral.com>
2019-12-18 18:44:24 +0200

    simplified logic, removed reset function

--- Languages/Javascript/Training/naughty-button/naughty.js
+++ Languages/Javascript/Training/naughty-button/naughty.js
@@ -1,57 +1,24 @@
 const unclickableButton = document.querySelector('.unclickable');
-let counter = 1;
 
 function getRandomIntInRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function setOffset(left, top) {
+function setRandomOffset() {
     return {
-        x: left,
-        y: top
-    }
-}
-
-function setRandomOffset(a, b) {
-    if (getRandomIntInRange(1, 3) === 1) {
-        return {
-            x: getRandomIntInRange(0, 50),
-            y: getRandomIntInRange(0, 50)
-        }
-    } else {
-        return {
-            x: getRandomIntInRange(50, 75),
-            y: getRandomIntInRange(50, 75)
-        }
-    }
-}
-
-function positionReset() {
-    const reset = setOffset(25, 25);
-    unclickableButton.style.left = reset.x + "%";
-    unclickableButton.style.top = reset.y + "%";
-}
-
-function handleOffset() {
-    counter++;
-
-    if (counter === 5) {
-        counter = 1;
-        positionReset();
-
-    } else {
-        updateOffset();
+        x: getRandomIntInRange(0, 75),
+        y: getRandomIntInRange(0, 75)
     }
 }
 
 function updateOffset() {
-    const offset = setRandomOffset(2, 3);
+    const offset = setRandomOffset();
     unclickableButton.style.left = offset.x + "%";
     unclickableButton.style.top = offset.y + "%";
 }
 
-unclickableButton.addEventListener("mouseover", handleOffset);
+unclickableButton.addEventListener("mouseover", updateOffset);
 
 
-setInterval(handleOffset, 500);
+setInterval(updateOffset, 500);
 
