8d5a86cd3b7243e5a1c7b4a4f799792ced4bef7b
Asaf Ravona <asaf.ravona@ladbrokescoral.com>
2019-12-30 11:25:09 +0200

    added in progress all ways to cut a word

---
+++ Languages/Python/all_ways_to_cut.py
@@ -0,0 +1,14 @@
+def all_ways_to_cut(letters, num_words):
+    print(type(letters))
+    numberOfLetters = len(letters)
+    print(numberOfLetters)
+
+    lettersToList = list(letters)
+
+    for letter in lettersToList:
+        print(letter[i])
+
+    print(lettersToList)
+    print(num_words)
+
+all_ways_to_cut("doggy", 3)
