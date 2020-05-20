e51ee5dc442031e500d9df6ac2e2ca57d9e5dced
Asaf <asafravona@gmail.com>
2020-05-20 14:59:18 +0300

    minor fixes

--- Languages/Javascript/Laurence Svekis/Practise JavaScript by Building 3 Applications from Scratch/Dice_Game/app.js
+++ Languages/Javascript/Laurence Svekis/Practise JavaScript by Building 3 Applications from Scratch/Dice_Game/app.js
@@ -1,81 +1,73 @@
-const rollButton = document.getElementById("roll");
-let players = document.getElementById("players");
-let rolls = document.getElementById("rolls");
-let winner = document.getElementById("winner");
+"use strict";
+
+const rollBtn = document.querySelector(".roll");
+let players = document.querySelector("#players");
+let rolls = document.querySelector("#rolls");
+let winner = document.querySelector("#winner");
 let title = document.querySelector(".title");
 let numberOfPlayers = 0;
 
 class Player {
-    constructor(index, roll) {
-        this.index = index;
-        this.roll = roll;
-    }
+  constructor(index, roll) {
+    this.index = index;
+    this.roll = roll;
+  }
 }
 
-// creates an element and appending data inside of it
 function renderItem(element, data) {
-    let item = document.createElement("div");
-    let itemContent = document.createTextNode(data);
-    item.appendChild(itemContent);
-    element.appendChild(item);
+  let item = document.createElement("div");
+  let itemContent = document.createTextNode(data);
+  item.appendChild(itemContent);
+  element.appendChild(item);
 }
 
-function roll(num) {
-    return Math.floor(Math.random() * num + 1);
+function getRandomInt(num) {
+  return Math.floor(Math.random() * num + 1);
 }
 
-//formats roll number to be displayed as a visual symbol using unicode
 function formatResult(rollNumber) {
-    let rollInUnicode = 9855 + rollNumber;
-    rollInUnicode = rollInUnicode.toString();
-    rollInUnicode = String.fromCodePoint(rollInUnicode);
-    return rollInUnicode;
+  let rollInUnicode = 9855 + rollNumber;
+  rollInUnicode = rollInUnicode.toString();
+  rollInUnicode = String.fromCodePoint(rollInUnicode);
+  return rollInUnicode;
 }
 
-// resets game
 function resetGame() {
-    players.innerHTML = "";
-    rolls.innerHTML = "";
-    winner.innerHTML = "";
+  players.textContent = "";
+  rolls.textContent = "";
+  winner.textContent = "";
 }
 
 function handleRollClick() {
-    const results = [];
-    let result;
-    result = document.createTextNode(result);
-    let highestRoll = 0;
-    let winningPlayer = '';
-    winner.style.display = "flex";
+  const results = [];
+  let result;
+  result = document.createTextNode(result);
+  let highestRoll = 0;
+  let winningPlayer = '';
+  winner.style.display = "flex";
 
-    if (!numberOfPlayers) {
-        numberOfPlayers = prompt("How many players?");
+  if (!numberOfPlayers) {
+    numberOfPlayers = prompt("How many players?");
+  }
+
+  resetGame();
+
+  for (let i = 1; i <= numberOfPlayers; i++) {
+    let currentPlayer = new Player(`Player ${i}`, getRandomInt(6));
+    renderItem(players, currentPlayer.index);
+    renderItem(rolls, formatResult(currentPlayer.roll));
+    results.push(currentPlayer.roll);
+
+    if (currentPlayer.roll > highestRoll) {
+      highestRoll = currentPlayer.roll;
+      winningPlayer = currentPlayer.index;
+    } else if (currentPlayer.roll === highestRoll) {
+      winningPlayer = "Nobody";
     }
+  }
 
-    resetGame();
-
-    // rendering players count (left column)
-    for (let i = 1; i <= numberOfPlayers; i++) {
-        // create a div for each player inside players
-        let currentPlayer = new Player(`Player ${i}`, roll(6));
-        renderItem(players, currentPlayer.index);
-        renderItem(rolls, formatResult(currentPlayer.roll));
-
-        // push each roll to results array for later use of choosing a winner
-        results.push(currentPlayer.roll);
-
-        // choosing a winner
-        if (currentPlayer.roll > highestRoll) {
-            highestRoll = currentPlayer.roll;
-            winningPlayer = currentPlayer.index;
-        } else if (currentPlayer.roll === highestRoll) {
-            winningPlayer = "Nobody";
-        }
-    }
-
-
-    let roundWinner = document.createTextNode(winningPlayer + " wins");
-    winner.appendChild(roundWinner);
+  let roundWinner = document.createTextNode(winningPlayer + " wins");
+  winner.appendChild(roundWinner);
 }
 
-// Adding an event listener for our roll button
-rollButton.addEventListener("click", handleRollClick);
+rollBtn.addEventListener("click", handleRollClick);
