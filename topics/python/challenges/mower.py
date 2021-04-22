2cfb936fb48b972f22adbf0c7f3f5e23b85d1021
Asaf Ravona <asafravona@gmail.com>
2021-04-22 22:57:21 +0300

    cleaned up comments and redudant code

--- topics/python/challenges/mower.py
+++ topics/python/challenges/mower.py
@@ -1,29 +1,32 @@
 class Lawn:
-    def __init__(self, size):
-        self.size = size
-        self.grid = []
-        self.width = self.size[0]
-        self.length = self.size[1]
+    def __init__(self, width, length) -> None:
+        self._grid = []
+        self.width = width
+        self.length = length
 
     def render_grid(self):
-        x_axis = [col for col in range(self.width)]
-        print(x_axis)
-        y_axis = [row for row in range(self.length)]
-        print(y_axis)
-        for i, y in enumerate(y_axis):
-            self.grid.append([])
-            for x in x_axis:
-                self.grid[i].append((x,y))
+        x_axis = [x for x in range(self.width)]
+        y_axis = [y for y in range(self.length)]
 
-        self.grid = reversed(self.grid)
-        for row in self.grid:
+        for i, y in enumerate(y_axis):
+            self._grid.append([])
+            for x in x_axis:
+                self._grid[i].append((x, y))
+
+        self._grid = reversed(self._grid)
+        for row in self._grid:
             print(row)
+
+
+lawn = Lawn(width=5, length=5)
+lawn.render_grid()
+
 
 class Mower:
     def __init__(self, position, orientation, directions):
         self._position = position
         self._orientation = orientation
-        self._directions = directions
+        self._directions = list(directions)
 
     def get_position(self):
         return self._position
@@ -38,36 +41,68 @@ class Mower:
         self._orientation = orientation
 
     def get_directions(self):
-        return self._directions
+        for step in self._directions:
+            return step
 
     def move(self):
         for step in self._directions:
-            if step=="F":
-                print('moving forward', self.get_position())
-            else:
-                self.set_orientation(step)
-                new_orientation = self.get_orientation()
-                print('new orientation', new_orientation)
+            [x, y] = self.get_position()
+            # print(
+            #     f"current position: {[x,y]} | step {step} | current orientation {self._orientation}"
+            # )
+            if step == "F":
+                # set new position according to current orientation:
+                if self.get_orientation() == "N":
+                    self.set_position([x, min(lawn.length, y + 1)])
+                    # print("new position set successfully", self._position)
 
+                elif self.get_orientation() == "E":
+                    self.set_position([min(x + 1, lawn.width), y])
+                    # print("new position set successfully", self._position)
+
+                elif self.get_orientation() == "S":
+                    self.set_position([x, max(y - 1, 0)])
+                    # print("new position set successfully", self._position)
+
+                elif self.get_orientation() == "W":
+                    self.set_position([max(0, x - 1), y])
+                    # print("new position set successfully", self._position)
+
+                # don't forget logic for lawn borders
+
+            else:
+                if self._orientation == "N":
+                    if step == "L":
+                        self.set_orientation("W")
+                    else:
+                        self.set_orientation("E")
+
+                elif self._orientation == "E":
+                    if step == "L":
+                        self.set_orientation("N")
+                    else:
+                        self.set_orientation("S")
+
+                elif self._orientation == "S":
+                    if step == "L":
+                        self.set_orientation("E")
+                    else:
+                        self.set_orientation("W")
+
+                elif self._orientation == "W":
+                    if step == "L":
+                        self.set_orientation("S")
+                    else:
+                        self.set_orientation("N")
 
 
 mower1 = Mower(position=[1, 2], orientation="N", directions="LFLFLFLFF")
+mower2 = Mower(position=[3, 3], orientation="E", directions="FFRFFRFRRF")
+
 mower1.move()
-print(f"mower directions: {mower1.get_directions()}")
-print(f"current mower position: {mower1.get_position()}")
-print(f"current mower orientation: {mower1.get_orientation()}")
+print(f"final mower position: {mower1.get_position()}")
+print(f"final mower orientation: {mower1.get_orientation()}")
 
-
-lawn1 = Lawn([5, 4])
-print(lawn1.render_grid())
-
-# TEST
-# This file is given in input:
-# 5 5
-# 1 2 N
-# LFLFLFLFF
-# 3 3 E
-# FFRFFRFRRF
-# This output is expected (final positions of mowers):
-# 1 3 N
-# 5 1 E
+mower2.move()
+print(f"final mower position: {mower2.get_position()}")
+print(f"final mower orientation: {mower2.get_orientation()}")
